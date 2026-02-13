445870f7dcfdbe7f2860f37eb7c788969a3084fa
b33lz3bub <147434853+r4iden-IITR@users.noreply.github.com>
2026-02-13 22:49:39 +0530

    Issue infering RAM fixed added internal forwarding and reduced read latency

--- regfile.v
+++ regfile.v
@@ -1,24 +1,24 @@
 module regfile(
     input clk,
-    input reset,
     input write_en,
     input [4:0] rs1, rs2, write_reg,
     input [31:0] write_data,
-    output reg [31:0] a,b
+    output [31:0] a,b
 );
     integer i;
     reg [31:0] register [0:31];
     always @(posedge clk) begin
-        if (reset) begin
-            for (i = 0; i < 32; i++) begin
-                register[i] <= 32'b0;
-            end
-        end else if (write_en && write_reg != 5'b0) begin
+        if (write_en && write_reg != 5'b0) begin
             register[write_reg] <= write_data;
-        end else begin
-            register[write_reg] <= register[write_reg];
         end
-        a <= register[rs1];
-        b <= register[rs2];
     end
+    assign a =
+        (rs1 == 5'd0) ? 32'b0 :
+        (write_en && write_reg == rs1) ? write_data :
+        register[rs1];
+
+    assign b =
+        (rs2 == 5'd0) ? 32'b0 :
+        (write_en && write_reg == rs2) ? write_data :
+        register[rs2];
 endmodule
