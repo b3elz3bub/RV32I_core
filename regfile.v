6c5e000c6d6dcce5c10e7c94191baca1d7c4e2de
b33lz3bub <147434853+r4iden-IITR@users.noreply.github.com>
2026-02-13 22:21:58 +0530

    modified data types to be compatible with Verilog-2001 standard

--- regfile.v
+++ regfile.v
@@ -4,13 +4,13 @@ module regfile(
     input write_en,
     input [4:0] rs1, rs2, write_reg,
     input [31:0] write_data,
-    output [31:0] a,b
+    output reg [31:0] a,b
 );
-
+    integer i;
     reg [31:0] register [0:31];
     always @(posedge clk) begin
         if (reset) begin
-            for (int i = 0; i < 32; i++) begin
+            for (i = 0; i < 32; i++) begin
                 register[i] <= 32'b0;
             end
         end else if (write_en && write_reg != 5'b0) begin
