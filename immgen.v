f3e867a24382a2450751032e1543c86dc268ec19
b33lz3bub <147434853+r4iden-IITR@users.noreply.github.com>
2026-02-18 11:47:42 +0530

    mod to use internal decoding

--- immgen.v
+++ immgen.v
@@ -1,16 +1,20 @@
 module immgen(
     input [31:0] inst,
-    input [2:0] imm_sel,
     output reg [31:0] imm
 );
     always @(*) begin
-        case (imm_sel)
-            3'b000: imm = { {20{inst[31]}}, inst[31:20] };                            // I-type
-            3'b001: imm = { {20{inst[31]}}, inst[31:25], inst[11:7] };                // S-type
-            3'b010: imm = { {20{inst[31]}},inst[7], inst[30:25], inst[11:8], 1'b0 };  // B-type
-            3'b011: imm = { {12{inst[31]}}, inst[19:12],inst[20],inst[30:21],1'b0 };  // J-type
-            3'b100: imm = { {12{inst[31]}}, inst[31:12] };                            // U-type
-            default: imm = 32'b0;
+        case (inst[6:2])
+            5'b00000, 5'b00011, 5'b00100, 5'b11001, 5'b11100 : 
+                imm = { {20{inst[31]}}, inst[31:20] };                             // I-type
+            5'b01000: 
+                imm = { {20{inst[31]}}, inst[31:25], inst[11:7] };                 // S-type
+            5'b11000: 
+                imm = { {20{inst[31]}}, inst[7], inst[30:25], inst[11:8], 1'b0 };  // B-type
+            5'b11011:
+                imm = { {12{inst[31]}}, inst[19:12], inst[20], inst[30:21],1'b0 }; // J-type
+            5'b00101, 5'b01101:
+                imm = { inst[31:12], 12{1'b0} };                                   // U-type
+            default: imm = 32'bx;
         endcase
     end
 endmodule
