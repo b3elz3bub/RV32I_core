1da1bcd066178e20ef9509fe7933d5882f8d7263
Mayank <mayankrajj1010@gmail.com>
2026-04-18 03:13:23 +0530

    Update pc.v

--- 5_stage/srcs/core/pc.v
+++ 5_stage/srcs/core/pc.v
@@ -2,7 +2,7 @@
 module pc (
 	input 					clk,
 	input 					rst,
-    input                   stall,    // <-- ADDED THIS
+    input                   stall,    // added STALL
 	input 		[31:0] 		pc_next,
 	output reg 	[31:0] 		pc_out
 );
@@ -10,7 +10,7 @@ module pc (
 	always @(posedge clk or posedge rst) begin
 		if (rst) begin
 			pc_out <= `RESET_VECTOR;
-		end else if (!stall) begin    // <-- ADDED THIS
+		end else if (!stall) begin    
 			pc_out <= pc_next;
 		end
         // If stall is high, do nothing (keep current pc_out value)
