539eff896f3c27288994e01b8a54e3ead23385bb
Mayank <mayankrajj1010@gmail.com>
2026-04-18 03:12:58 +0530

    Update pc.v

--- 5_stage/srcs/core/pc.v
+++ 5_stage/srcs/core/pc.v
@@ -1,17 +1,18 @@
 `include "params.vh"
 module pc (
-	input 				clk,
-	input 				rst,
-	input               stall,
-	input 		[31:0] 	pc_next,
-	output reg 	[31:0] 	pc_out
+	input 					clk,
+	input 					rst,
+    input                   stall,    // <-- ADDED THIS
+	input 		[31:0] 		pc_next,
+	output reg 	[31:0] 		pc_out
 );
 
 	always @(posedge clk or posedge rst) begin
 		if (rst) begin
 			pc_out <= `RESET_VECTOR;
-		end else if (!stall) begin 
+		end else if (!stall) begin    // <-- ADDED THIS
 			pc_out <= pc_next;
 		end
+        // If stall is high, do nothing (keep current pc_out value)
 	end
 endmodule
