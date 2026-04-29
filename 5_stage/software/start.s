9e9497f2bd52975f1bd205455fb4ebf0232df5fd
Mayank <mayankrajj1010@gmail.com>
2026-04-29 21:07:45 +0530

    stack pointer changed to initialize at 0x42000

--- 5_stage/software/start.s
+++ 5_stage/software/start.s
@@ -5,7 +5,7 @@ _start:
     # 1. Initialize the Stack Pointer (x2 / sp)
     # The stack grows downwards. Set to top of Data Memory.
     # RAM is now 8KB: 0x0000 – 0x1FFF, so SP = 0x2000.
-    li sp, 0x00002000
+    li sp, 0x00042000
 
     # 2. Call the C main function
     jal ra, main
